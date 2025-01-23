d4b8cbad02b61d8c5e6d90180190f7fc3defd29a
August Hindenes <ahind@proton.me>
2025-01-23 21:25:30 +0100

    Added docker test and prod environments

--- web-app/backend/backend/settings.py
+++ web-app/backend/backend/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# For static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
