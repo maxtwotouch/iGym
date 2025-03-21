22b79bbc1b05c8c2ebc4826c16fc3da6a6f2dbd9
VikingTheDev <augshin2002@gmail.com>
2025-03-21 12:02:45 +0100

    Created fresh frontend project from template

--- web-app/frontend/vite.config.ts
+++ web-app/frontend/vite.config.ts
@@ -1,21 +1,8 @@
-import { defineConfig } from "vite"
-import autoprefixer from "autoprefixer"
-import tailwindcss from "tailwindcss"
-import { reactRouter } from "@react-router/dev/vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [
-    /**
-     * IMPORTANT: Set `ssr: true` so that the build includes
-     * both client (build/client) and server (build/server) bundles.
-     */
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
